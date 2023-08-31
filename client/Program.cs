4885fae5612aeda17aae87fb5f5f24c8354c5edc
Jonatan Lifshits <djonli2003@gmail.com>
2023-08-31 23:03:45 +0300

    socket tcp testing

---
+++ client/Program.cs
@@ -0,0 +1,45 @@
+﻿
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System;
+
+const string serverIp = "127.0.0.1";
+const int port = 8080;
+
+Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Parse(serverIp), port);
+
+try
+{
+    Console.WriteLine("Enter your message: ");
+    string? message = Console.ReadLine();
+
+    if (message is not null) 
+    {
+        await socket.ConnectAsync(serverEndPoint);
+        await socket.SendAsync(Encoding.UTF8.GetBytes(message));
+
+        byte[] buffer = new byte[256];
+        int byteCount = 0;
+        string response = string.Empty;
+
+        do
+        {
+            byteCount = await socket.ReceiveAsync(buffer);
+            response += Encoding.UTF8.GetString(buffer, 0, byteCount);
+
+        } while (socket.Available > 0);
+
+        Console.WriteLine($"Response: {response}");
+
+        socket.Shutdown(SocketShutdown.Both);
+        socket.Close();
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Error:{ex.Message}");
+}
+
+Console.ReadLine();
---
+++ socket_practise/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+const string localHost = "127.0.0.1";
+const int port = 8080;
+
+Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse(localHost), port);
+
+try
+{
+    socket.Bind(endPoint);
+    socket.Listen();
+
+    Console.WriteLine($"Server started at {localHost}:{port}");
+
+    while (true) 
+    {
+        Socket remotrSocket = await socket.AcceptAsync();
+        Console.WriteLine("Connection opened");
+
+        byte[] buffer = new byte[256];
+        int byteCount = 0;
+        string message = string.Empty;
+
+        do
+        {
+            byteCount = await remotrSocket.ReceiveAsync(buffer);
+            message += Encoding.UTF8.GetString(buffer, 0, byteCount);
+
+        } while (remotrSocket.Available > 0);
+
+        Console.WriteLine($"{DateTime.Now.ToShortTimeString()} : {message}");
+
+        string response = Console.ReadLine();
+        remotrSocket.Send(Encoding.UTF8.GetBytes(response));
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Error:{ex.Message}");
+}
+
